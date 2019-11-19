c3901f9ba47a1e799d414623854bfe254de5fbde
Mikhail Davydov <mdavydov@griddynamics.com>
2019-11-19 21:28:36 +0300

    Unfortunate fixes.

--- src/main/docker/spark-submit.sh
+++ src/main/docker/spark-submit.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+mv -f ${SPARK_APPLICATION_JAR_LOCATION} ${SPARK_SHARED_JAR_LOCATION}
+
 /spark/bin/spark-submit \
 --class ${SPARK_APPLICATION_MAIN_CLASS} \
 --master ${SPARK_MASTER_URL} \
 --deploy-mode cluster \
 --total-executor-cores 1 \
  ${SPARK_SUBMIT_ARGS} \
- ${SPARK_APPLICATION_JAR_LOCATION} \
+ ${SPARK_SHARED_JAR_LOCATION} \
  ${SPARK_APPLICATION_ARGS}
