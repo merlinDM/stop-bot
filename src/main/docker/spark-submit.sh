3d0150e839e4b2f471f9483e178be03219d027d1
Mikhail Davydov <mdavydov@griddynamics.com>
2019-11-21 17:12:35 +0300

    Add docker-compose, fix issues with resources, shadow core Spark libs.

--- src/main/docker/spark-submit.sh
+++ src/main/docker/spark-submit.sh
@@ -1,12 +1,20 @@
-#!/bin/bash
+#!/bin/bash -e
 
-mv -f ${SPARK_APPLICATION_JAR_LOCATION} ${SPARK_SHARED_JAR_LOCATION}
+if mv -f ${SPARK_APPLICATION_JAR_LOCATION} ${SPARK_SHARED_JAR_LOCATION} ; then
+  echo "INFO: Successfully moved $SPARK_APPLICATION_JAR_LOCATION to $SHARED_DIR"
 
-/spark/bin/spark-submit \
---class ${SPARK_APPLICATION_MAIN_CLASS} \
---master ${SPARK_MASTER_URL} \
---deploy-mode cluster \
---total-executor-cores 1 \
- ${SPARK_SUBMIT_ARGS} \
- ${SPARK_SHARED_JAR_LOCATION} \
- ${SPARK_APPLICATION_ARGS}
+  /spark/bin/spark-submit \
+  --class ${SPARK_APPLICATION_MAIN_CLASS} \
+  --master ${SPARK_MASTER_URL} \
+  --deploy-mode client \
+  --total-executor-cores 1 \
+   ${SPARK_SUBMIT_ARGS} \
+   ${SPARK_SHARED_JAR_LOCATION} \
+   ${SPARK_APPLICATION_ARGS}
+else
+  ERROR_CODE=$?
+
+  echo "ERROR: JAR NOT FOUND"
+
+  exit ${ERROR_CODE}
+fi
